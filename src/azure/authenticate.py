1b0c147d565e0871a431fe83c6bcdbf7e6fe92e2
NguyenHuuDangKhoa <nhdkhoa1994@gmail.com>
2023-08-31 15:12:08 -0600

    Delete files after refactoring them into deployment.py

--- src/azure/authenticate.py
+++
@@ -1,9 +0,0 @@
-from azureml.core import Workspace
-
-def authenticate():
-    try:
-        ws = Workspace.from_config()
-        print(ws)
-        return ws
-    except:
-        print('Workspace not found')
